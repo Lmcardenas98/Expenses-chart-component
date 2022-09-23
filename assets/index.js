2e06c347d5db7137d14fc06572257c961fdf4a67
Lmcardenas98 <lmcardenas.981106@gmail.com>
2022-09-23 01:18:11 -0400

    first comit

---
+++ assets/index.js
@@ -0,0 +1,42 @@
+const bars = document.querySelector('.grafic-bars');
+
+function paintBars() {
+    fetch('../data.json')
+        .then(a => a.json())
+        .then(b => {
+            
+            b.forEach(element => {
+            bars.innerHTML += `
+            <div class='grafic-bars-days'>
+                <div class='grafic-bars-price hidden'>$${element.amount}</div>
+                <div class='individual-bar' 
+                    style='height:${element.amount}%;
+                            width:30px;
+                            background-color: ${element.amount <= 50 ? 'rgb(225, 116, 96)' : 'rgb(127, 180, 186)'} ;'>
+                </div>
+                <div class='small-letters'>${element.day}</div>
+            </div>`   
+        });
+    })
+}
+
+
+
+
+paintBars();
+
+setTimeout(() => {
+        const individualBars = document.querySelectorAll('.individual-bar');
+
+        individualBars.forEach(ele => {
+            ele.addEventListener('mouseover' , (e) => {
+                e.path[1].childNodes[1].classList.remove('hidden')
+            })
+        })
+
+        individualBars.forEach(ele => {
+            ele.addEventListener('mouseout' , (e) => {
+                e.path[1].childNodes[1].classList.add('hidden')
+            })
+        })
+    },100)
